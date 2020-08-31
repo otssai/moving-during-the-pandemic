1e3f70d1f618abaac1332bb44a552ccaa0750f3f
Xiang Yu <yuxiang@Xiangs-MacBook-Air.local>
2020-08-31 15:10:22 -0400

    helloworld

---
+++ moving-during-the-pandemic/src/main/java/org/launchcode/movingduringthepandemic/MovingDuringThePandemicApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.movingduringthepandemic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingDuringThePandemicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingDuringThePandemicApplication.class, args);
+	}
+
+}
